0046e1cfa2e4a366c029202afc63b2705c42415f
Toni Oriol <tonioriol@gmail.com>
2025-06-03 20:17:21 +0200

    Refactor GoToCat script for better readability and maintainability
    - Extract configuration into centralized CONFIG object
    - Replace inline HTML string with DOM manipulation for notification
    - Improve function separation and naming (generateUrlCandidates, handleNotification, tryRedirect)
    - Fix logical bug in isTargetLanguage (removed meaningless || true condition)
    - Add better variable naming and code organization
    - Clean up language file dependencies and remove unused files

---
+++ lang-codes.js
@@ -0,0 +1,58 @@
+var langCodes = [
+  {
+    "alpha3_b": "slo",
+    "alpha3_t": "slk",
+    "alpha2": "sk",
+    "english": ["Slovak"],
+    "french": ["slovaque"]
+  },
+  {
+    "alpha3_b": "sun",
+    "alpha3_t": null,
+    "alpha2": "su",
+    "english": ["Sundanese"],
+    "french": ["soundanais"]
+  },
+  {
+    "alpha3_b": "tut",
+    "alpha3_t": null,
+    "alpha2": null,
+    "english": ["Altaic languages"],
+    "french": ["alta\u00efques, langues"]
+  },
+  {
+    "alpha3_b": "arg",
+    "alpha3_t": null,
+    "alpha2": "an",
+    "english": ["Aragonese"],
+    "french": ["aragonais"]
+  },
+  {
+    "alpha3_b": "asm",
+    "alpha3_t": null,
+    "alpha2": "as",
+    "english": ["Assamese"],
+    "french": ["assamais"]
+  },
+  {
+    "alpha3_b": "lim",
+    "alpha3_t": null,
+    "alpha2": "li",
+    "english": ["Limburgan", "Limburger", "Limburgish"],
+    "french": ["limbourgeois"]
+  },
+  {
+    "alpha3_b": "ltz",
+    "alpha3_t": null,
+    "alpha2": "lb",
+    "english": ["Luxembourgish", "Letzeburgesch"],
+    "french": ["luxembourgeois"]
+  },
+  {
+    "alpha3_b": "mac",
+    "alpha3_t": "mkd",
+    "alpha2": "mk",
+    "english": ["Macedonian"],
+    "french": ["mac\u00e9donien"]
+  }
+]
